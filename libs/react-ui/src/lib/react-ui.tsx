5c0d0baf8dbebd3c49408f9cff53c7cf875baef1
Petr Glaser <syreanis+1@gmail.com>
2023-06-09 22:00:36 +0200

    feat(ui): add react ui lib

---
+++ libs/react-ui/src/lib/react-ui.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ReactUiProps {}
+
+export function ReactUi(props: ReactUiProps) {
+  return (
+    <div>
+      <h1>Welcome to ReactUi!</h1>
+    </div>
+  )
+}
+
+export default ReactUi
