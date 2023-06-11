b7cd647965bf22c88bb062990eb8e9f5ed927611
Petr Glaser <syreanis+1@gmail.com>
2023-06-11 22:42:47 +0200

    fix(builder): use recommended fragment solution

--- libs/builder/src/lib/fragment.tsx
+++ libs/builder/src/lib/fragment.tsx
@@ -1,19 +1,12 @@
-import { Slot, component$ } from '@builder.io/qwik'
+import { Fragment, component$ } from '@builder.io/qwik'
 import { RegisteredComponent } from '@builder.io/sdk-qwik'
 
-export const Fragment = component$(() => {
-  return <Slot />
+export const FragmentAlt = component$((props) => {
+  return <Fragment {...props} />
 })
 
 export const FragmentBuilder: RegisteredComponent = {
-  component: Fragment,
+  component: FragmentAlt,
   name: 'Core:Fragment',
-
-  inputs: [
-    {
-      name: 'text',
-      type: 'string',
-      defaultValue: 'Hello world',
-    },
-  ],
+  canHaveChildren: true,
 }
